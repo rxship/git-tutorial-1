940f9cdf065b72fcbe51abb532ac116ce0859ab5
Rushikesh <rushipatel.8688@gmail.com>
2025-10-14 23:12:24 -0400

    initial commit version 1

---
+++ index.js
@@ -0,0 +1,17 @@
+function print(name){
+    console.log("hello " + name);
+}
+
+
+//testing out the function
+print("world");
+
+function total(n){
+    let sum = 0;
+    for(let i=0; i<=n; i++){
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(total(5)); // should print 15
